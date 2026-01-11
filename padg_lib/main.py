ad68a0ed263969872ab5f5fd3620fe1f794087c9
Michal-IO <u56169@student.wat.edu.pl>
2026-01-11 20:09:32 +0100

    feat: added database connection

--- padg_lib/main.py
+++ padg_lib/main.py
@@ -1,7 +1,15 @@
 from padg_lib.model import airports, employees, clients
 from padg_lib.controller import *
 from padg_lib.view import root
+import psycopg2
 
+db_engine = psycopg2.connect(
+    user="postgres",
+    database="postgres",
+    password="postgres",
+    port="5432",
+    host="localhost",
+)
 
 button_airports.config(command=switch_airports)
 button_employees.config(command=switch_employees)
@@ -13,5 +21,14 @@ button_filtr.config(command=filter)
 
 listbox_list.bind('<<ListboxSelect>>', select)
 
+load_db(db_engine)
+def close():
+    save_db(db_engine)
+    db_engine.close()
+    root.destroy()
+
+root.protocol("WM_DELETE_WINDOW", close)
+
 if __name__ == '__main__':
+    load_db(db_engine)
     root.mainloop()
